86140be26da764f4f4079264f4728de0b6cc10b1
Brianna Veenstra <b.g.veenstra@gmail.com>
2015-07-23 11:55:51 -0700

    auth

--- scripts.js
+++ scripts.js
@@ -3,23 +3,27 @@ $(function() {
   console.log('I\'m here to serve')
 
   // var baseUrl = "http://localhost:3000" // DEV
-  var baseUrl = "https://pickup-wdi.herokuapp.com" // PRD
-  // DEFINE LINES
-  
+  // var baseUrl = "https://pickup-wdi.herokuapp.com" // PRD
+
+  // don't need a base url for now, we'll just blank it
+  // it defaults to whichever of the above is correct
+  var baseUrl = "";  
+
+  // compile line template function (calling it $line)
   $line = _.template( $("#lineTemplate").html() )
 
   $.get(baseUrl + '/api/lines', function(data) {
     var lines = data  
 
     _.each(lines, function(line) {
-      console.log(line)
+      //console.log(line)
       $('#lines').append($line(line))
     })
   })
 
   $('#new-line').submit(function(e){
     e.preventDefault();
-    console.log("im submitting a form")
+    // console.log("im submitting a form")
     var line = {
       text: $('#line-text').val()
     }
@@ -28,8 +32,62 @@ $(function() {
       $('#lines').prepend($line(data))
     })
 
-  })
-})
+  });
+
+  // @AUTH
+  // submit event listener for login form
+  $('#login-form').on("submit", function(event){
+    // don't forget to event.preventDefault()
+    event.preventDefault();
+    // get data out of form
+    var userData = {
+      email: $("#login-user-email").val(),
+      password: $("#login-user-password").val()
+    };
+    // send login request with form data
+    $.post('/login', userData, function(response){
+      // console.log("got log in response ", response);
+      // once server responds, update logged in message 
+      updateViewForCurrentUser();
+    });
+  });
+
+  // @AUTH @NEW
+  // for efficiency, let's go ahead and set up jQuery variables
+  // for selecting dom elements we'll want to use over and over
+  var $loggedInMessage = $("#loggedInMessage");
+  var $logoutButton = $("#logout-btn");
+
+  // @AUTH
+  // update the logged in message on the page
+  // This function is called after login and when the page loads.   
+  // Would be called after signup too if we were using jQuery to submit signup form. 
+  function updateViewForCurrentUser(){
+    // request current user info from server
+    $.get('/currentuser', function(response){
+        // server responds with the current user
+        console.log("server says current user is: ", response);
+        if (response === null){
+          // no one is logged in
+          $loggedInMessage.html("You're definitely not logged in.");
+          // make sure the logout button is hidden
+          $logoutButton.hide();
+        } else {
+          // someone is logged in
+          $loggedInMessage.html("You're definitely logged in as " + response.email + ".");
+          // show the logout button
+          $logoutButton.show();
+        }
+      }
+    );
+  }
+
+  // @AUTH
+  // check current user on page load to update logged in message
+  updateViewForCurrentUser();
+
+
+});
 
 
 
--- server.js
+++ server.js
@@ -5,8 +5,20 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose');
 
+// require express-session for authentication (stores current user)
+var session = require('express-session');
+
+// configure session
+app.use(session({
+  saveUninitialized: true,
+  resave: true,
+  secret: 'SuperSecretCookie',
+  cookie: { maxAge: 60000 }
+}));
+
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/pickup');
 var Line = require('./line');
+var User = require('./user');
 
 // OPEN THE API TO REQUESTS FROM ANY DOMAIN
 app.use(cors());
@@ -14,28 +26,121 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname));
 
+
+// MIDDLEWARE
+// custom middleware to manage our sessions
+app.use('/', function (req, res, next) {
+  // saves userId in session for logged-in user
+  req.login = function (user) {
+    req.session.userId = user._id; 
+    console.log("login: user id " + req.session.userId )
+  };
+
+  // finds user currently logged in based on `session.userId`
+  req.currentUser = function (callback) {
+    User.findOne({_id: req.session.userId}, function (err, user) {
+      console.log("finding current user from session ", req.session);
+      if (!user){
+        callback("could not find user", null);
+      } else {  
+        req.user = user;
+        console.log("found current user: ", user);
+        callback(null, user);
+      }
+    });
+  };
+
+  // destroy `session.userId` to log out user
+  req.logout = function () {
+    req.session.userId = null;
+    req.user = null;
+  };
+
+  next();  // required for middleware
+});
+
 app.get('/', function(req, res) {
   var index = __dirname + "/index.html";
   res.sendFile(index);
 });
 
+
+// @AUTH
+// USER#CREATE
+// create new user with secure password and log user in automatically
+app.post('/api/users', function (req, res) {
+  console.log("server received signup form data: ", req.body.user);
+  var newUser = req.body.user;
+  User.createSecure(newUser, function (err, user) {
+    // log user in automatically when created
+    req.login(user);
+    //res.json(user); // can send user json to test...
+    res.redirect('/'); // but for real, want to redirect to home page
+  });
+});
+
+// @AUTH
+// authenticate user and set session
+app.post('/login', function (req, res) {
+    // from client:
+    // var userData = {
+    //   email: $("#login-user-email").val(),
+    //   password: $("#login-user-password").val()
+    // };
+  console.log("server received login form data: ", 
+    req.body.email, req.body.password);
+
+  User.authenticate(req.body.email, req.body.password, function (err, user) {
+    if (user){
+      req.login(user);
+      console.log("authenticated user: ", user);
+
+      //res.redirect('/profile');  // <- ideally redirect to a profile view
+      res.json(user);
+    } else {
+      // send the client
+      // whatever error came from the authentication code
+      res.status(500).send(err);
+    }
+  });
+});
+
+
+// @AUTH
+// see who the current user is 
+app.get('/currentuser', function (req, res) {
+  // check for current (logged-in) user
+  // @BUG during demo, Brianna was using req.login here  :(
+  req.currentUser(function(err, user) {
+    // once the user is found in the db, send it back
+    res.json(user);
+  });
+});
+
+// once we can tell who current user is:
+// - change the front end based on looking it up
+// - add stuff / delete stuff only if the user is logged in
+
+
+
 // LINES#QUERY
 app.get('/api/lines', function(req, res) {
-  console.log(Line);
+  //console.log(Line);
   Line.find().sort('-_id').exec(function(err, lines) {
-    console.log(lines);
+    //console.log(lines);
     res.json(lines);
   });
 });
 
 // LINES#CREATE
 app.post('/api/lines', function(req, res) {
-  // SAVE LINE TO DB
+  // make new line
   var line = new Line({
     text: req.body.text
   });
-
+  // save line to db
   line.save(function(err, line) {
+    // once saved, send back the line
     res.json(line);
   });
 });
